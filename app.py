21aa00d296495bdfd6a8ab79ab6929c783827a84
belleh71513 <belleh715313@gmail.com>
2022-04-30 15:49:57 +0800

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
 from model.s3 import upload_file_to_s3, AWS_STORAGE_BUCKET_NAME
+from model.connect_mysql import insert_message, get_message
 import os
 
 
@@ -8,7 +9,6 @@ app = Flask(__name__, static_folder="static", static_url_path="/")
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
 
-AWS_STORAGE_BUCKET_NAME = "wehelp-third-phase-week1"
 AWS_CLOUD_FRONT = "https://dk9dkz6ihqn2o.cloudfront.net"
 
 
@@ -16,14 +16,45 @@ AWS_CLOUD_FRONT = "https://dk9dkz6ihqn2o.cloudfront.net"
 def index():
   return render_template("index.html")
 
-@app.route("/upload", methods=["POST"])
-def upload():
-  file = request.files["file"]
-  file.save(os.path.join("uploads", file.filename))
-  upload_file(f"uploads/{file.filename}", AWS_STORAGE_BUCKET_NAME)
-  return
+@app.route("/files", methods=["POST"])
+def upload_file():
+  message = request.form.get("message")
+  image = request.files["file"]
+  try:
+    upload_file_to_s3(image, AWS_STORAGE_BUCKET_NAME, image.filename)
+  except:
+    res = {
+      "error" : True,
+      "message" : "AWS S3 伺服器錯誤"
+    }
+    print("upload file to s3 error")
+    return jsonify(res), 500
 
+  image_url = f"https://dk9dkz6ihqn2o.cloudfront.net/{image.filename}"
+  try:
+    insert_message(message, image_url)
+  except:
+    res = {
+      "error" : True,
+      "message" : "RDS 伺服器錯誤"
+    }
+    print("upload file to s3 error")
+    return jsonify(res), 500
 
+  return jsonify({"ok" : True}), 200
+
+@app.route("/files", methods=["GET"])
+def get_file():
+  try:
+    files = get_message()
+    print(files)
+  except:
+    res = {
+      "error" : True,
+      "message" : "RDS 伺服器錯誤"
+    }
+    return jsonify(res), 500
+  return jsonify({"data" : files}), 200
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=3000)
